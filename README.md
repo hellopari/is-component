e9892833c8c8307564b46d7e44f1a647c9eaed15
zhujiaojiao <zhujiaojiao@dm-ai.com>
2021-08-11 15:09:27 +0800

    上传本地代码

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # is-component
+
+vue3，<component :is="currentTabComponent" class="tab"></component>的用法
+避坑： vite.config.ts的配置
---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+<div id="dynamic-component-demo" class="demo">
+  <button
+     v-for="tab in tabs"
+     :key="tab"
+     :class="['tab-button', { active: currentTab === tab }]"
+     @click="currentTab = tab"
+   >
+    {{ tab }}
+  </button>
+
+  <component :is="currentTabComponent" class="tab"></component>
+</div>
+</template>
+
+<script>
+export default{
+    data() {
+      return {
+        currentTab: 'Home',
+        tabs: ['Home', 'Posts', 'Archive']
+      }
+    },
+    computed: {
+      currentTabComponent() {
+        return 'tab-' + this.currentTab.toLowerCase()
+      }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
